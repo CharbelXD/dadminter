4642c3872665910ae48d78f47d4c1fbba3f1621e
CharbelXD <120786321+CharbelXD@users.noreply.github.com>
2025-02-24 16:10:04 +0200

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3LK6iKw2KFy5oxS-wIS-rohtMph0uTss",
-  authDomain: "solana-token-launcher-d1a80.firebaseapp.com",
-  projectId: "solana-token-launcher-d1a80",
-  storageBucket: "solana-token-launcher-d1a80.firebasestorage.app",
-  messagingSenderId: "812445619534",
-  appId: "1:812445619534:web:679f6dfb53121587323a1f",
-  measurementId: "G-L5N0JDQTPV"
+  apiKey: "AIzaSyAKxtFfZRSar5vboGA2u0uvf27seYajwAM",
+  authDomain: "minter-demo.firebaseapp.com",
+  projectId: "minter-demo",
+  storageBucket: "minter-demo.firebasestorage.app",
+  messagingSenderId: "478022043797",
+  appId: "1:478022043797:web:2a9d7f5c856e877f4df39c",
+  measurementId: "G-37D47T6S6R"
 };
 
 // Initialize Firebase
