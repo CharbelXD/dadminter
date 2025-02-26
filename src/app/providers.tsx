800ef81f79b6f1dd796f9a61fe40cafdb2f79309
CharbelXD <120786321+CharbelXD@users.noreply.github.com>
2025-02-26 11:45:11 +0200

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,11 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import {
   PhantomWalletAdapter,
   SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
-import { Adapter } from "@solana/wallet-adapter-base"; // ✅ Correct import for wallet adapters
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 
@@ -15,15 +14,11 @@ export interface ProvidersProps {
 }
 
 export function Providers({ children }: ProvidersProps) {
-  const [wallets, setWallets] = useState<Adapter[]>([]); // ✅ Corrected type for wallet adapters
-
-  useEffect(() => {
-    setWallets([new PhantomWalletAdapter(), new SolflareWalletAdapter()]);
-  }, []);
+  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter()], []);
 
   return (
-    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
-      <WalletProvider wallets={wallets} autoConnect={false}>
+    <ConnectionProvider endpoint="https://api.devnet.solana.com">
+      <WalletProvider wallets={wallets} autoConnect={true}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
